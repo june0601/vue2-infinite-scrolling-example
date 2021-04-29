0b7d711b2bf6a181d52308669a33cc93f3676634
hope <hope@T0DAA029>
2021-04-29 14:01:41 +0800

    vue2-infinite-scrolling-example

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# vue2-infinite-scrolling-example
+# vue-infinite-scrolling-example
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### vue2 exercise from ...
+https://www.digitalocean.com/community/tutorials/vuejs-implementing-infinite-scroll
---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="app">
+    <h1>Random User</h1>
+      <div
+         class="user"
+         v-for="user in users"
+         :key="user.first"
+      >
+      <div class="user-avatar">
+        <img :src="user.picture.large" />
+      </div>
+      <div class="user-details">
+        <h2 class="user-name">
+          {{ user.name.first }}
+          {{ user.name.last }}
+        </h2>
+        <ul>
+          <li><strong>Birthday:</strong> {{ formatDate(user.dob.date) }}</li>
+          <li><strong>Location:</strong> {{ user.location.city }}, {{ user.location.state }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  methods: {
+    getInitialUsers() {
+      axios.get(`https://randomuser.me/api/?results=5`).then((response) => {
+        this.users = response.data.results;
+      });
+    },
+    formatDate(dateString) {
+      let convertedDate = new Date(dateString);
+      return convertedDate.toDateString();
+    },
+    getNextUser() {
+      window.onscroll = () => {
+        let bottomOfWindow = document.documentElement.scrollTop + window.innerHeight === document.documentElement.offsetHeight;
+        if (bottomOfWindow) {
+          axios.get(`https://randomuser.me/api/`).then(response => {
+            this.users.push(response.data.results[0]);
+          });
+        }
+      }
+    }
+  },
+  beforeMount() {
+    this.getInitialUsers();
+  },
+  mounted() {
+    this.getNextUser();
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.user {
+  display: flex;
+  background: #ccc;
+  border-radius: 1em;
+  margin: 1em auto;
+}
+
+.user-avatar {
+  padding: 1em;
+}
+
+.user-avatar img {
+  display: block;
+  width: 100%;
+  min-width: 64px;
+  height: auto;
+  border-radius: 50%;
+}
+
+.user-details {
+  padding: 1em;
+}
+
+.user-name {
+  margin: 0;
+  padding: 0;
+  font-size: 2rem;
+  font-weight: 900;
+}
+</style>
